5530c905d3228095bd287dae68f68226f7f088b3
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2022-09-29 11:58:55 -0300

    chore: added basic styles and assets structures and files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+import { App } from './App'
+
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
