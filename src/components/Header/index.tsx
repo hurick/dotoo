a160cc43734f0b08bf58d03a8e182d9cd9bbd96b
Hurick Krügner <14249327+hurick@users.noreply.github.com>
2022-09-29 15:56:01 -0300

    feat(Header): finished structure and style component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import iconRocket from '../../assets/rocket.svg'
+
+import styles from './Header.module.css'
+
+export const Header = () => {
+  const { header, h__logo, hl__title, hlt__detail } = styles;
+
+  return (
+    <header className={header}>
+      <div className={h__logo}>
+        <img src={iconRocket} alt="Rocket" />
+
+        <h1 className={hl__title}>
+          do<span className={hlt__detail}>too</span>
+        </h1>
+      </div>
+    </header>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { App } from './App'
+import { Home } from './pages/Home'
 
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <Home />
   </StrictMode>
 )
